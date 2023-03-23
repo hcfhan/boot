e9c9ba95abb1adb38f51cfb9a676ebb926a157e7
hcf <hcf@hcf.com>
2023-03-23 16:20:08 +0800

    hcf_second

--- springboot1/src/main/java/com/example/springboot1/pojo/Per.java
+++ springboot1/src/main/java/com/example/springboot1/pojo/Per.java
@@ -3,5 +3,6 @@ package com.example.springboot1.pojo;
 public class Per {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test");
     }
 }
